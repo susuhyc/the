9c730ee059ffa11bf71879f94734c5a21923dce6
LISUSU <liss@staff.chinabyte.com>
2017-10-16 15:28:09 +0800

    初始化项目结构

---
+++ src/main/java/com/susuhyc/userinfo/controller/UserInfoControl.java
@@ -0,0 +1,11 @@
+package com.susuhyc.controller.userinfocontrol;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+@Controller
+public class UserInfoControl {
+
+}
---
+++ src/main/java/com/susuhyc/userinfo/dao/userInfoDao.java
@@ -0,0 +1,7 @@
+package com.susuhyc.dao;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+public class userInfoDao {
+}
